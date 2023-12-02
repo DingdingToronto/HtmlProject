d120a20bd188ca75072b3f4e106670dff7821a00
DingdingToronto <jiabaoding6@gmail.com>
2023-12-02 13:24:51 -0500

    begin page added

--- script.js
+++ script.js
@@ -2,101 +2,164 @@ $(window).on("load", function () {
   let count = 0;
   var ballInterval;
 
-  // Set up the initial interval to create and animate balls
+  var introductionText =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit cumque ipsam, asperiores reiciendis libero fuga dolorem? Quod pariatur ad cum";
+  var words = introductionText.split(" ");
+  var index = 0;
+
+  var intervalId = setInterval(() => {
+    if (index < words.length) {
+      $(".introduction").text($(".introduction").text() + " " + words[index]);
+      index++;
+    } else {
+      clearInterval(intervalId); // Stop the setInterval once all words are displayed
+      $("<button class=beginButton>Lets begin</button>").appendTo(
+        ".beforeGame"
+      );
+    }
+  }, 100);
+
   setInterval(function () {
-    var randomX = Math.random() * ($(window).width() - 100);
-    var randomColor1 = Math.random() * 255;
-    var randomColor2 = Math.random() * 255;
-    var randomColor3 = Math.random() * 255;
-    var shadowColor =
-      "rgb(" +
-      Math.floor(randomColor1) +
-      ", " +
-      Math.floor(randomColor2) +
-      ", " +
-      Math.floor(randomColor3) +
-      ")";
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
 
-    // Create a new ball element and append it to the body
-    $("<div class='ball'><span class='fa fa-star star'></span></div>")
-      .appendTo(".game")
+    var minDistanceFromEdge = 50; // Adjust this value as needed
+
+    var positionStarX =
+      Math.random() * (windowWidth - 2 * minDistanceFromEdge) +
+      minDistanceFromEdge;
+    var positionStarY =
+      Math.random() * (windowHeight - 2 * minDistanceFromEdge) +
+      minDistanceFromEdge;
+
+    var degreeStar = Math.random() * 360;
+
+    $("<div class='stars'><span class='fa fa-star star'></span></div>")
+      .appendTo(".beforeGame")
       .css({
-        left: randomX + "px",
-        color: shadowColor,
-        backgroundColor: "transparent",
-        boxShadow: "0 0 30px 15px " + shadowColor,
+        position: "absolute",
+        left: positionStarX + "px",
+        top: positionStarY + "px",
+        color: "yellow",
+        textShadow: "0 0 10px white",
+        fontSize: "100px",
+        height: "100px",
+        width: "100px",
+        transform: "rotateZ(" + degreeStar + "deg)",
+        animation: "stars 1s linear infinite",
       });
-
-    // Check if count is greater than or equal to 5 and adjust the width
-    if (count >= 5) {
-      $(".ball").css("animation", "move 1s linear infinite");
-      $("body").css("animation", "changeColor2 0.5s infinite alternate");
-    }
-    if (count >= 10) {
-      $(".ball").css("animation", "move 0.7s linear infinite");
-      $("body").css("animation", "changeColor 5s infinite alternate");
-    }
   }, 1000);
 
-  // Set up a timeout to start the interval for removing balls after 3 seconds
-  setTimeout(function () {
-    // Set up the interval to remove the first ball every second
-    ballInterval = setInterval(function () {
-      $(".ball:first").remove();
+  setTimeout(() => {
+    var ballInterval = setInterval(function () {
+      $(".stars:first").remove();
     }, 1000);
   }, 1000);
 
-  // Use event delegation for click events on dynamically created elements
-  $("body").on("click", ".ball", function () {
-    var ball = $(this);
+  $(".beginButton").on("click", function () {
+    $(".beforeGame").css("display", "none");
+    $(".game").css("display", "block");
+  });
+  $(".beginButton").on("click", function () {
+    //game begins----------------------------------------------------------------------------------------------------------------------------->
 
-    // Increase the size of the ball
-    ball.css({
-      width: "100px",
-      height: "100px",
-      opacity: "0.3",
-      color: "yellow",
-      fontSize: "200px",
-      textShadow: "0 0 100px 60px yellow",
+    $(".beforeGame").css("display", "none");
+    $(".game").css("display", "block");
+    // Set up the initial interval to create and animate balls
+    setInterval(function () {
+      var randomX = Math.random() * ($(window).width() - 100);
+      var randomColor1 = Math.random() * 255;
+      var randomColor2 = Math.random() * 255;
+      var randomColor3 = Math.random() * 255;
+      var shadowColor =
+        "rgb(" +
+        Math.floor(randomColor1) +
+        ", " +
+        Math.floor(randomColor2) +
+        ", " +
+        Math.floor(randomColor3) +
+        ")";
+
+      // Create a new ball element and append it to the body
+      $("<div class='ball'><span class='fa fa-star star'></span></div>")
+        .appendTo(".game")
+        .css({
+          left: randomX + "px",
+          color: shadowColor,
+          backgroundColor: "transparent",
+          boxShadow: "0 0 30px 15px " + shadowColor,
+        });
+
+      // Check if count is greater than or equal to 5 and adjust the width
+      if (count >= 5) {
+        $(".ball").css("animation", "move 1s linear infinite");
+      }
+      if (count >= 10) {
+        $(".ball").css("animation", "move 0.7s linear infinite");
+      }
+    }, 1000);
+
+    // Set up a timeout to start the interval for removing balls after 3 seconds
+    setTimeout(function () {
+      // Set up the interval to remove the first ball every second
+      ballInterval = setInterval(function () {
+        $(".ball:first").remove();
+      }, 1000);
+    }, 1000);
+
+    // Use event delegation for click events on dynamically created elements
+    $("body").on("click", ".ball", function () {
+      var ball = $(this);
+
+      // Increase the size of the ball
+      ball.css({
+        width: "100px",
+        height: "100px",
+        opacity: "0.3",
+        color: "yellow",
+        fontSize: "200px",
+        textShadow: "0 0 100px 60px yellow",
+      });
+
+      // Hide the ball after 300 milliseconds
+      setTimeout(function () {
+        ball.css("display", "none");
+      }, 150);
+
+      count++;
+
+      // Update the score
+      $(".score").html("Count now is: " + count);
     });
 
-    // Hide the ball after 300 milliseconds
     setTimeout(function () {
-      ball.css("display", "none");
-    }, 150);
+      $(".game").css("display", "none");
 
-    count++;
-
-    // Update the score
-    $(".score").html("Count now is: " + count);
-  });
-
-  setTimeout(function () {
-    $(".game").css("display", "none");
-    setTimeout(function () {
-      $(".result").html(
-        "<div>Time is over. Your score is : " + count + "</div>"
-      );
-      $(".result").fadeIn(2000);
-      $(".result").css("display", "block");
       setTimeout(function () {
-        $(".button").fadeIn(1000);
-      }, 1000);
-    }, 200);
-  }, 20000);
-  //time control
-  setInterval(function () {
-    $(".timesheet").css("left", "-=5px");
-  }, 100);
-  $(".button").on("click", function () {
-    location.reload();
+        $(".result").html(
+          "<div>Time is over. Your score is : " + count + "</div>"
+        );
+        $(".result").fadeIn(2000);
+        $(".result").css("display", "block");
+        setTimeout(function () {
+          $(".button").fadeIn(1000);
+        }, 1000);
+      }, 200);
+    }, 5000);
+    //time control
+    setInterval(function () {
+      $(".timesheet").css("left", "-=5px");
+    }, 100);
+    $(".button").on("click", function () {
+      location.reload();
+    });
+    setTimeout(function () {
+      $(".timesheet").css("backgroundColor", "#f39c12");
+    }, 10000);
+    setTimeout(function () {
+      $(".timesheet").css("backgroundColor", "#e74c3c");
+      $(".time").css("border", "2px solid #ff0000");
+      $(".time").css("animation", "blink 0.5s infinite");
+    }, 15000);
   });
-  setTimeout(function () {
-    $(".timesheet").css("backgroundColor", "#f39c12");
-  }, 10000);
-  setTimeout(function () {
-    $(".timesheet").css("backgroundColor", "#e74c3c");
-    $(".time").css("border", "2px solid #ff0000");
-    $(".time").css("animation", "blink 0.5s infinite");
-  }, 15000);
 });
